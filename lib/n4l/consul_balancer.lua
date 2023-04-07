896723b82f2c224efddb4daa1edee3584c2aea55
hulining <loserkid@163.com>
2023-04-07 12:17:26 +0800

    尝试使用 package.load 支持 upstream server 配置权重

--- lib/n4l/consul_balancer.lua
+++ lib/n4l/consul_balancer.lua
@@ -2,11 +2,14 @@
 --
 --
 --
+-- 简单说明: 尝试使用 https://github.com/openresty/lua-resty-balancer 与 共享内存 来实现 upstream server 支持权重
+-- 其中，权重的配置是通过注册服务时配置 https://github.com/hashicorp/consul/blob/v1.4.0/api/agent.go#L67-L70 传入的
 
 -- Dependencies
 local http = require "resty.http"
 local balancer = require "ngx.balancer"
 local json = require "cjson"
+local resty_roundrobin = require "resty.roundrobin"
 
 local WATCH_RETRY_TIMER = 0.5
 
@@ -20,11 +23,13 @@ local _M = new_tab(0, 5) -- Change the second number.
 _M.VERSION = "0.04"
 _M._cache = {}
 
+-- 校验 consul uri
 local function _sanitize_uri(consul_uri)
   -- TODO: Ensure that uri has <proto>://<host>[:<port>] scheme
   return consul_uri
 end
 
+-- 定时器函数
 local function _timer(...)
   local ok, err = ngx.timer.at(...)
   if not ok then
@@ -32,6 +37,8 @@ local function _timer(...)
   end
 end
 
+-- 将 response 解析为 service.upstreams 对象. 
+-- [x] TODO: 需要将返回转化为 rr_up 对象
 local function _parse_service(response)
   if response.status ~= 200 then
     return nil, "bad response code: " .. response.status
@@ -46,13 +53,10 @@ local function _parse_service(response)
     return nil, "not trusting leaderless consul"
   end
   -- TODO: reuse some table?
-  local service = {}
   if not response.headers["X-Consul-Index"] then
     return nil, "missing consul index"
-  else
-    service.index = response.headers["X-Consul-Index"]
   end
-  service.upstreams = {}
+  local upstreams = {}
   for k, v in pairs(content) do
     local passing = true
     local checks = v["Checks"]
@@ -64,31 +68,20 @@ local function _parse_service(response)
     if passing then
       local s = v["Service"]
       local na = v["Node"]["Address"]
-      table.insert(service.upstreams, {
-          address = s["Address"] ~= "" and s["Address"] or na,
-          port = s["Port"],
-        })
+      
+      local address = s["Address"] ~= "" and s["Address"] or na
+      local port = s["Port"]
+      local weight = s["Weights"]["Passing"]
+
+      upstreams[address .. ":" .. port] = weight
+      ngx.log(ngx.INFO, "consul.balancer: add " .. address .. ":" .. port .. " to upstreams, weight " .. weight)
     end
   end
-  return service
+  local rr_up = resty_roundrobin:new(upstreams)
+  return rr_up
 end
 
-local function _persist(service_name, service)
-  if _M.shared_cache then
-    _M.shared_cache:set(service_name, json.encode(service))
-    return
-  end
-  _M._cache[service_name] = service
-end
-
-local function _aquire(service_name)
-  if _M.shared_cache then
-    local service_json = _M.shared_cache:get(service_name)
-    return service_json and json.decode(service_json) or nil
-  end
-  return _M._cache[service_name]
-end
-
+-- 构建向 consul 请求的 uri
 local function _build_service_uri(service_descriptor, service_index)
   local uri = _M._consul_uri .. "/v1/health/service/" .. service_descriptor.service
   local args = {
@@ -113,6 +106,8 @@ local function _build_service_uri(service_descriptor, service_index)
   return uri .. "?" .. ngx.encode_args(args)
 end
 
+-- 更新 service 对象属性
+-- [x] TODO: 更新 rr_up 对象属性
 local function _refresh(hc, uri)
   ngx.log(ngx.INFO, "consul.balancer: query uri: ", uri)
   local res, err = hc:request_uri(uri, {
@@ -122,14 +117,15 @@ local function _refresh(hc, uri)
     ngx.log(ngx.ERR, "consul.balancer: failed to refresh upstreams: ", err)
     return nil, err
   end
-  local service, err = _parse_service(res)
+  local rr_up, err = _parse_service(res)
   if err ~= nil then
     ngx.log(ngx.ERR, "consul.balancer: failed to parse consul response: ", err)
     return nil, err
   end
-  return service
+  return rr_up
 end
 
+-- 验证 service 对象传入的参数描述
 local function _validate_service_descriptor(service_descriptor)
   if type(service_descriptor) == "string" then
     service_descriptor = {
@@ -149,6 +145,8 @@ local function _validate_service_descriptor(service_descriptor)
 end
 
 -- signature must match nginx timer API
+-- watch 机制
+-- [x] TODO: 适配 rr_up 对象
 local function _watch(premature, service_descriptor)
   if premature then
     return nil
@@ -164,20 +162,22 @@ local function _watch(premature, service_descriptor)
   ngx.log(ngx.NOTICE, "consul.balancer: started watching for changes in ", service_descriptor.name)
   while true do
     local uri = _build_service_uri(service_descriptor, service_index)
-    local service, err = _refresh(hc, uri)
-    if service == nil then
+    local rr_up, err = _refresh(hc, uri)
+    if rr_up == nil then
       ngx.log(ngx.ERR, "consul.balancer: failed while watching for changes in ", service_descriptor.name, " retry scheduled")
       _timer(WATCH_RETRY_TIMER, _watch, service_descriptor)
       return nil
     end
+    -- 这里使用了 package.load 将 rr_up 对象保存下来
+    package.loaded[service_descriptor.name] = rr_up
     -- TODO: Save only newer data from consul to reduce GC load
-    service_index = service.index
-    _persist(service_descriptor.name, service)
     ngx.log(ngx.INFO, "consul.balancer: persisted service ", service_descriptor.name,
-                      " index: ", service_index, " content: ", json.encode(service))
+                      " index: ", service_index, " content: ", json.encode(rr_up))
+    ngx.sleep(5)
   end
 end
 
+-- 函数的主入口
 function _M.watch(consul_uri, service_list)
   -- start watching on first worker only, skip for others (if shared storage provided)
   if _M.shared_cache and ngx.worker.id() > 0 then
@@ -190,35 +190,28 @@ function _M.watch(consul_uri, service_list)
   end
 end
 
+-- 轮询 balancer 进行负载均衡
+-- [x] TODO: 取出 rr_up 对象,并进行负载均衡
 function _M.round_robin(service_name)
-  local service = _aquire(service_name)
-  if service == nil then
+  for k, v in pairs(package.loaded) do
+    ngx.log(ngx.INFO, "consul.balancer: package loaded: ",k)
+  end
+  -- 取出 rr_up 对象
+  local rr_up = package.loaded[service_name]
+  if rr_up == nil then
     ngx.log(ngx.ERR, "consul.balancer: no entry found for service: ", service_name)
     return ngx.exit(500)
   end
-  if service.upstreams == nil or #service.upstreams == 0 then
-    ngx.log(ngx.ERR, "consul.balancer: no peers for service: ", service_name)
-    return ngx.exit(500)
-  end
-  if service.state == nil or service.state > #service.upstreams then
-    service.state = 1
-  end
-  -- TODO: https://github.com/openresty/lua-resty-core/blob/master/lib/ngx/balancer.md#get_last_failure
-  -- set max tries only at first attempt
-  if not balancer.get_last_failure() then
-    balancer.set_more_tries(#service.upstreams - 1)
-  end
-  -- Picking next upstream
-  local upstream = service.upstreams[service.state]
-  service.state = service.state + 1
-  _persist(service_name, service)
-  local ok, err = balancer.set_current_peer(upstream["address"], upstream["port"])
+  
+  local server = rr_up:find()
+  local ok, err = balancer.set_current_peer(server)
   if not ok then
     ngx.log(ngx.ERR, "consul.balancer: failed to set the current peer: ", err)
     return ngx.exit(500)
   end
 end
 
+-- 设置共享内存名称
 function _M.set_shared_dict_name(dict_name)
   _M.shared_cache = ngx.shared[dict_name]
   if not _M.shared_cache then
